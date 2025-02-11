9221b0e4c241c664b07b517957b205af7d9ae1c8
makar10l <158200024+makar10l@users.noreply.github.com>
2025-02-11 22:35:52 +0300

    modified:   print.hpp

--- print.hpp
+++ print.hpp
@@ -1,12 +1,7 @@
 #include <string>
 #include <iostream>
 #include <cstring>
-void print(std::string str){   
-    int len = strlen(str);
-    if(str[len] == "n" && str[len--] == "\\"){
-        std::cout << str << std::endl;
-    }
-    else{
+#include <cstdlib>
+void print(std::string str){ 
         std::cout << str;
-    }
 }
